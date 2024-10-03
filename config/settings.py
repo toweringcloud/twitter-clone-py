e2309b97557c18fda80afd81ed2b5e0894f63f8d
toweringcloud <toweringcloud@gmail.com>
2024-10-03 22:12:23 +0900

    simple custom user authentication

--- config/settings.py
+++ config/settings.py
@@ -134,5 +134,12 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Auth
+# Authentication
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "config.authentication.TrustMeBroAuthentication",
+    ]
+}
