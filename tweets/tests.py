3946409b5efe4dca0f7c2bb0b5b3bc11108f5e8c
toweringcloud <toweringcloud@gmail.com>
2024-10-05 01:00:32 +0900

    django api test case on tweets crud

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,158 @@
-from django.test import TestCase
+# from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+from .models import Tweet
+
+
+class TestTweets(APITestCase):
+
+    PAYLOAD = "tweet paylowd"
+    PAYLOAD_NEW = "tweet paylowd new"
+    URL = "/api/v1/tweets/"
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+        user.set_password("1234")
+        user.save()
+        self.user = user
+
+        Tweet.objects.create(
+            payload=self.PAYLOAD,
+            user=self.user,
+        )
+
+    def test_get_tweets(self):
+        response = self.client.get(self.URL)
+        data = response.json()
+        print(data)
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status code isn't 200.",
+        )
+        self.assertIsInstance(
+            data,
+            list,
+            "response type isn't list",
+        )
+        self.assertEqual(
+            len(data),
+            1,
+            "data length isn't 1",
+        )
+        self.assertEqual(
+            data[0]["payload"],
+            self.PAYLOAD,
+            f"payload value isn't {self.PAYLOAD}",
+        )
+
+    def test_post_tweets(self):
+        self.client.force_login(
+            self.user,
+        )
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "payload": self.PAYLOAD_NEW,
+                "user": self.user,
+            },
+        )
+        data = response.json()
+        print(data)
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status code isn't 200.",
+        )
+        self.assertEqual(
+            data["payload"],
+            self.PAYLOAD_NEW,
+            f"payload value isn't {self.PAYLOAD_NEW}",
+        )
+
+
+class TestTweet(APITestCase):
+
+    PAYLOAD = "tweet paylowd"
+    PAYLOAD_UPDATE = "tweet paylowd update"
+    URL = "/api/v1/tweets/1"
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+        user.set_password("1234")
+        user.save()
+        self.user = user
+
+        Tweet.objects.create(
+            payload=self.PAYLOAD,
+            user=self.user,
+        )
+
+    def test_get_tweet(self):
+        response = self.client.get(
+            self.URL,
+            HTTP_ACCEPT="application/json",
+        )
+        data = response.json()
+        print(data)
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status code isn't 200.",
+        )
+        self.assertEqual(
+            data["payload"],
+            self.PAYLOAD,
+            f"payload value isn't {self.PAYLOAD}",
+        )
+
+    def test_put_tweet(self):
+        self.client.force_login(
+            self.user,
+        )
+
+        response = self.client.put(
+            self.URL,
+            data={
+                "payload": self.PAYLOAD_UPDATE,
+                "user": self.user,
+            },
+        )
+        data = response.json()
+        print(data)
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status code isn't 200.",
+        )
+        self.assertEqual(
+            data["payload"],
+            self.PAYLOAD_UPDATE,
+            f"payload value isn't {self.PAYLOAD_UPDATE}",
+        )
+
+    def test_delete_tweet(self):
+        self.client.force_login(
+            self.user,
+        )
+
+        response = self.client.delete(
+            self.URL,
+        )
+        print(response)
+
+        self.assertEqual(
+            response.status_code,
+            204,
+            "status code isn't 204.",
+        )
